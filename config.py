4cb6524629ee8ec2fd0d88a162d595d2119eb20d
Satyam Kumar Jha <satyamjha9911@gmail.com>
2024-11-27 01:37:41 +0530

    Mongo url

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ import os
 SECRET_KEY = "5369668e81a5406bbb0ac8edb235c753"
 
 # MongoDB URI for your database
-MONGO_URI = "mongodb://localhost:27017/comclass"
+MONGO_URI = "mongodb+srv://satyamjha9911:KOH4b6stO7WmHIJt@comclassdb.2moas.mongodb.net/?retryWrites=true&w=majority&appName=comclassdb"
